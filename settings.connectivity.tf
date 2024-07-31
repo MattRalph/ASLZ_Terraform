19affa7bc97ab4c679b2cfb58d0dabf1069ec629
MattRalph <Matt.Ralph@live.com.au>
2024-07-31 16:50:54 +1000

    versions updated, still threat_intelligence_allowlist issue

--- settings.connectivity.tf
+++ settings.connectivity.tf
@@ -89,7 +89,7 @@ locals {
                 base_policy_id                = ""
                 private_ip_ranges             = []
                 threat_intelligence_mode      = ""
-                threat_intelligence_allowlist = []
+                threat_intelligence_allowlist = [] # original [], tried {} https://github.com/Azure/terraform-azurerm-caf-enterprise-scale/issues/922
                 availability_zones = {
                   zone_1 = true
                   zone_2 = true
